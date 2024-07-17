f8fd0806bb3fa6709a56d49cd2eaf84d21def139
codeissimple <adam.jkirjacev@gmail.com>
2024-07-17 16:37:21 -0400

    2

--- list_files.py
+++ list_files.py
@@ -1,7 +1,20 @@
 import os
+import shutil
+
 # define the path to the target folder
 target_folder = os.path.expanduser("~/Downloads")
 # list all files in the target folder
-files = os.listdir(target_folder)
-# print the list of files
-print(files)
+folders = {
+    'Documents': ['.pdf', '.docx', '.txt'],
+    'Images': ['.jpg', '.jpeg', '.png', '.gif'],
+    'Videos': ['.mp4', '.mov', '.avi'],
+    'Audios': ['.mp3', '.wav']
+    'Archives': ['.zip', '.bz2', '.tar.gz', '.xz', '.rar', '.gz', '.bz2', '.tar.gz'],
+    'Installers': ['.exe', '.bat', '.batc'],
+    'Books': ['.epub', '.epubx', '.pptx'],
+    'Excel': ['.xls', '.xlsx', '.ppt'],
+    'HTML': ['.html', '.htm'],
+
+}
+
+}
