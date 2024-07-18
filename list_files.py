d12d36da2af9172072e7080d8777d592c300f0f1
codeissimple <adam.jkirjacev@gmail.com>
2024-07-17 20:17:26 -0400

    2

--- list_files.py
+++ list_files.py
@@ -14,7 +14,32 @@ folders = {
     'Books': ['.epub', '.epubx', '.pptx'],
     'Excel': ['.xls', '.xlsx', '.ppt'],
     'HTML': ['.html', '.htm'],
-
 }
 
-}
+# function to create folders if they don't exist
+
+def create_folder(base_path, folder_names):
+    for folder in folder_names:
+        folder_path = os.path.join(base_path, folder)
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+
+# Move files to their respective folders
+def move_files(base_path, files, folder_dict):
+    for file in files:
+        file_path = os.path.join(base_path, file)
+            if os.path.isfile(file_path):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
