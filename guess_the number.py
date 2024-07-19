e8e8467c87c23f9340a59db1e4ced96b01bffa01
codeissimple <adam.jkirjacev@gmail.com>
2024-07-18 21:22:17 -0400

    2

--- guess_the number.py
+++
@@ -1,8 +0,0 @@
-#guessing the number game
-import random
-
-secretNumber = random.randint(1,20)
-print("I'm thinking of a number between 1 and 20")
-
-# Ask the player to guess 6 times
-for guessesTaken in range(1,7):
